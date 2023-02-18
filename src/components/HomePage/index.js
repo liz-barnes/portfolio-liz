68570b1e7502a5389398af9c2c2b203826bf6397
Liz Barnes <embarnes95@gmail.com>
2023-02-17 16:10:06 -0800

    add paragraph under banner

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -7,6 +7,7 @@ export const HomePage = () => {
         <NavBar/>
         <div className='banner'>
             <h1>Designer & Frontend Developer</h1>
+            <p>I'm all about the visuals –– let's build something beautiful together</p>
         </div>
         <div className="bio">
 
