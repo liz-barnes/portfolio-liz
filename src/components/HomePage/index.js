f9ce51b58e9eb9309c626448e8b1dbf12c1d17c0
Liz Barnes <embarnes95@gmail.com>
2023-02-18 20:27:22 -0800

    add some content to skills cards and style

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -23,9 +23,37 @@ export const HomePage = () => {
         <div className="skills">
             <div className="skills-card">
                 <h5>Designer</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. </p>
+                <h6>What I design:</h6>
+                <ul>
+                    <li>Websites</li>
+                    <li>Graphics</li>
+                    <li>Logos</li>
+                    <li>Personalized Gifts</li>
+                    <li>Jersey Designs</li>
+                </ul>
+                <h6>Tools:</h6>
+                <ul>
+                    <li>Figma</li>
+                </ul>
             </div>
             <div className="skills-card">
                 <h5>Frontend Developer</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. </p>
+                <h6>Languages:</h6>
+                <ul>
+                    <li>JS</li>
+                    <li>React</li>
+                    <li>HTML</li>
+                    <li>CSS / SASS</li>
+                    <li>Typsescript</li>
+                </ul>
+                <h6>Tools:</h6>
+                <ul>
+                    <li>VSCode</li>
+                    <li>Postman</li>
+                    <li>Figma</li>
+                </ul>
             </div>
 
         </div>
