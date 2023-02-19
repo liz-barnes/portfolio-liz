09ee5f8a2d1318109c72524351dc833d0f1b5099
Liz Barnes <embarnes95@gmail.com>
2023-02-18 19:47:19 -0800

    update and style headshot

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,5 +1,6 @@
 import { NavBar } from "../NavBar"
 import './index.scss'
+import lizHeadshot from '../../images/liz-headshot.png'
 
 export const HomePage = () => {
   return (
@@ -10,6 +11,13 @@ export const HomePage = () => {
             <p>I'm all about the visuals –– let's build something beautiful together</p>
         </div>
         <div className="bio">
+            <div className="headshot">
+                <img className="image" src={lizHeadshot} alt='headshot of liz'/>
+
+            </div>
+            <div className="intro">
+
+            </div>
 
         </div>
         <div className="skills">
