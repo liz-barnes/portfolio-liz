46accd89d82f017c5dfc33fa77a3d7899c957ab8
Liz Barnes <embarnes95@gmail.com>
2023-02-21 11:27:52 -0800

    install free solid svg, add envelope and style socials

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import './index.scss'
 
@@ -8,6 +9,9 @@ export const Footer = () => {
       <div className="socials">
         <a className="icon-link" href="/">
           <FontAwesomeIcon className="icon" icon={faGithub} />
+        </a>
+        <a className="icon-link" href="/">
+          <FontAwesomeIcon className="icon" icon={faEnvelope} />
         </a>
       </div>
       <p></p>
