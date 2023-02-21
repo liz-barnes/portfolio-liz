b3e0bc074605fff68cbcef6d10190d7f089a920f
Liz Barnes <embarnes95@gmail.com>
2023-02-21 11:58:21 -0800

    create mock projects data

--- src/components/DevelopmentPage/index.js
+++ src/components/DevelopmentPage/index.js
@@ -1,6 +1,18 @@
 import { NavBar } from "../NavBar";
 import {Footer} from "../Footer";
 
+const projects = [
+    {
+        title: 'This Portfolio',
+        type: 'website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    },
+    {
+        title: `Nashville 'Shine`,
+        type: 'website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    }
+]
 export const DevelopmentPage = () => {
     return (
         <div>
