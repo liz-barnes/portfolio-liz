5c12438ea4421cadff39a31a0889f674317863d3
Liz Barnes <embarnes95@gmail.com>
2023-02-22 15:59:34 -0800

    create design project mock data

--- src/components/DesignPage/index.js
+++ src/components/DesignPage/index.js
@@ -1,6 +1,27 @@
 import { Footer } from '../Footer'
 import { NavBar } from '../NavBar'
 
+const projects = [
+    {
+      title: `Nashville Nightshade`,
+      type: 'Sports Graphics & Social Media Content',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    },
+    {
+        title: `That Legendary Play`,
+        type: 'Company Graphics & Logo Design',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    },
+    {
+        title: `The Rodgers Group`,
+        type: 'Logo Design',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    },
+  ];
+  
 export const DesignPage = () => {
   return (
     <>
