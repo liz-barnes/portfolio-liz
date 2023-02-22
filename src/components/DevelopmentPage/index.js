15dab6857fcb55d53e2f67489e03265e613ff07f
Liz Barnes <embarnes95@gmail.com>
2023-02-22 15:45:11 -0800

    add more projects

--- src/components/DevelopmentPage/index.js
+++ src/components/DevelopmentPage/index.js
@@ -20,7 +20,24 @@ const projects = [
     githubLink: 'https://github.com/liz-barnes/nashville-shine-ultimate',
     websiteLink: 'https://nashvilleshineultimate.netlify.app/',
   },
+  {
+    title: `Adventure Planner`,
+    type: 'website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    githubLink: 'https://github.com/liz-barnes/front-end-capstone',
+    websiteLink: 'https://adventure-planner-lb.netlify.app',
+  },
+  {
+    title: `Vanlife Sanity`,
+    type: 'website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+    githubLink: 'https://github.com/liz-barnes/backend-capstone2.0',
+    websiteLink: 'https://vanlife-sanity.netlify.app',
+  },
 ]
+
 export const DevelopmentPage = () => {
   return (
     <div>
