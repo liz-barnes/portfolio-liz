77ab9dd82c0e22fe235538fe507ad5b362251806
Liz Barnes <embarnes95@gmail.com>
2023-02-22 14:47:32 -0800

    create structure and style for project description

--- src/components/DevelopmentPage/index.js
+++ src/components/DevelopmentPage/index.js
@@ -1,32 +1,39 @@
-import { NavBar } from "../NavBar";
-import {Footer} from "../Footer";
-import './index.scss';
+import { NavBar } from '../NavBar'
+import { Footer } from '../Footer'
+import './index.scss'
 
 const projects = [
-    {
-        title: 'This Portfolio',
-        type: 'website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
-    },
-    {
-        title: `Nashville 'Shine`,
-        type: 'website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
-    }
+  {
+    title: 'This Portfolio',
+    type: 'website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+  },
+  {
+    title: `Nashville 'Shine`,
+    type: 'website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+  },
 ]
 export const DevelopmentPage = () => {
-    return (
-        <div>
-            <NavBar />
-            <div className="projects">
-                {projects.map((proj) => (
-                    <div className="project-container">
-                        <h3>{proj.title}</h3>
-                        <h6>{proj.type}</h6>
-                    </div>
-                ))}
+  return (
+    <div>
+      <NavBar />
+      <div className="projects">
+        {projects.map((proj) => (
+          <div className="project-container">
+            <div className="project-content">
+              <h3>{proj.title}</h3>
+              <h6>{proj.type}</h6>
+              <div className="project-description">
+                I am shown when someone hovers over the div above.
+              </div>
             </div>
-            <Footer />
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  )
 }
