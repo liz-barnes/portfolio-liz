ce4c1585856b7ff1948013d11919852ac62e78d0
Liz Barnes <embarnes95@gmail.com>
2023-02-22 15:54:36 -0800

    add NavBar and Footer

--- src/components/DesignPage/index.js
+++ src/components/DesignPage/index.js
@@ -1,7 +1,12 @@
+import { Footer } from '../Footer'
+import { NavBar } from '../NavBar'
+
 export const DesignPage = () => {
   return (
     <>
+      <NavBar />
       <h1>Design Page</h1>
+      <Footer />
     </>
   )
 }
