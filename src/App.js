7417bdb6ed3abd784482c669329e09eaedc41e98
Liz Barnes <embarnes95@gmail.com>
2023-02-22 15:53:29 -0800

    create design component and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { DevelopmentPage } from './components/DevelopmentPage';
+import { DesignPage } from './components/DesignPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/development" element={<DevelopmentPage />} />
+        <Route path="/design" element={<DesignPage />} />
       </Routes>
     </>
   );
---
+++ src/components/DesignPage/index.js
@@ -0,0 +1,7 @@
+export const DesignPage = () => {
+  return (
+    <>
+      <h1>Design Page</h1>
+    </>
+  )
+}
