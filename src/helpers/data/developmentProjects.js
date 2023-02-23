0a2490715cd7cb9f4344d4b730e05424e6c72ea9
Liz Barnes <embarnes95@gmail.com>
2023-02-23 14:21:48 -0800

    more styles to tools-section

--- src/helpers/data/developmentProjects.js
+++ src/helpers/data/developmentProjects.js
@@ -1,4 +1,4 @@
-import {faReact, faFigma} from '@fortawesome/free-brands-svg-icons'
+import {faReact, faFigma, faSquareJs, faFontAwesomeAlt } from '@fortawesome/free-brands-svg-icons'
 
 export const projects = [
   {
@@ -8,7 +8,7 @@ export const projects = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
     githubLink: 'https://github.com/liz-barnes/portfolio-liz',
     websiteLink: 'https://lizmarie-portfolio.netlify.app',
-    tools: [faReact, faFigma]
+    tools: [faReact, faFigma, faSquareJs, faFontAwesomeAlt]
   },
   {
     title: `Nashville 'Shine`,
