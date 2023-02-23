dd468f9b06e9470d73c8095fd6a756f108b7062b
Liz Barnes <embarnes95@gmail.com>
2023-02-22 16:13:30 -0800

    add NavBar and Footer

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,12 @@
+import { Footer } from '../Footer'
+import { NavBar } from '../NavBar'
+
 export const Resume = () => {
   return (
     <>
+      <NavBar />
       <h1>Resume</h1>
+      <Footer />
     </>
   )
 }
