f6aec7d6dd71381b359de8532a3c99880b47a774
Liz Barnes <embarnes95@gmail.com>
2023-02-22 16:18:57 -0800

    add resume pdf, structure resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,27 @@
 import { Footer } from '../Footer'
 import { NavBar } from '../NavBar'
+import resume from '../../images/liz_barnes_resume.pdf'
 
 export const Resume = () => {
   return (
     <>
       <NavBar />
-      <h1>Resume</h1>
+      <div className="resume-page">
+        <h3 className="outro">
+          Thank you for visiting my site and checking out my work! Please reach
+          out –– I would love to collaborate, talk code or anything in between.
+        </h3>
+        <div className="pdf-button-container">
+          <a
+            className="pdf-link"
+            href={resume}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Click here to view/download my resume!
+          </a>
+        </div>
+      </div>
       <Footer />
     </>
   )
