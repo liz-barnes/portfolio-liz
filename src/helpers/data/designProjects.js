5768d5b5448031e28abdadd89e8f7387db81427f
Liz Barnes <embarnes95@gmail.com>
2023-02-23 14:59:47 -0800

    update description for TLP

--- src/helpers/data/designProjects.js
+++ src/helpers/data/designProjects.js
@@ -9,7 +9,7 @@ const projects = [
     title: `That Legendary Play`,
     type: 'Company Graphics & Logo Design',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
+      'That Legendary Play is a sports print retailer that offers prints and other media displaying legendary sports plays and diagrams. I designed their logo and layout for their graphics. I created their initial handful of graphics representing unforgettable plays in football.',
   },
   {
     title: `The Rodgers Group`,
