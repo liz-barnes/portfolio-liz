6e454bb94a432a32b93b7947f7f106b59983d226
Liz Barnes <embarnes95@gmail.com>
2023-02-22 16:40:01 -0800

    add github link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ export const Footer = () => {
   return (
     <div className="footer">
       <div className="socials">
-        <a className="icon-link" href="/">
+        <a className="icon-link" href="https://github.com/liz-barnes" target={"_blank"} rel="noreferrer">
           <FontAwesomeIcon className="icon" icon={faGithub} />
         </a>
         <a className="icon-link" href="https://www.linkedin.com/in/lizmbarnes/" target={"_blank"} rel="noreferrer">
