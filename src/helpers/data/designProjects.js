09d0771f94e6c3720f37d93768e1dca593437d43
Liz Barnes <embarnes95@gmail.com>
2023-02-23 13:34:25 -0800

    add designProjects to helpers folder

--- src/helpers/data/designProjects.js
+++ src/helpers/data/designProjects.js
@@ -1,35 +1,21 @@
-export const projects = [
+const projects = [
   {
-    title: 'This Portfolio',
-    type: 'website',
+    title: `Nashville Nightshade`,
+    type: 'Sports Graphics & Social Media Content',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
-    githubLink: 'https://github.com/liz-barnes/portfolio-liz',
-    websiteLink: 'https://lizmarie-portfolio.netlify.app',
   },
   {
-    title: `Nashville 'Shine`,
-    type: 'website',
+    title: `That Legendary Play`,
+    type: 'Company Graphics & Logo Design',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
-    githubLink: 'https://github.com/liz-barnes/nashville-shine-ultimate',
-    websiteLink: 'https://nashvilleshineultimate.netlify.app/',
   },
   {
-    title: `Adventure Planner`,
-    type: 'website',
+    title: `The Rodgers Group`,
+    type: 'Logo Design',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
-    githubLink: 'https://github.com/liz-barnes/front-end-capstone',
-    websiteLink: 'https://adventure-planner-lb.netlify.app',
-  },
-  {
-    title: `Vanlife Sanity`,
-    type: 'website',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur tortor a malesuada feugiat. Pellentesque fermentum quis neque eget faucibus. Sed commodo euismod elit facilisis finibus. Curabitur sed accumsan felis. Donec sollicitudin mi non consectetur hendrerit. Aliquam erat volutpat. Etiam tortor leo, mattis sed tellus vitae, interdum vulputate orci.',
-    githubLink: 'https://github.com/liz-barnes/backend-capstone2.0',
-    websiteLink: 'https://vanlife-sanity.netlify.app',
   },
 ];
 
