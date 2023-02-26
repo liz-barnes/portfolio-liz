77259e4dedcae2f6b9d2c40e2ff2facb9be2c588
Liz Barnes <embarnes95@gmail.com>
2023-02-25 20:19:34 -0800

    add navbar menu-icon, style menu-icon to be responsive

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,8 +1,12 @@
 import { Link, NavLink } from 'react-router-dom'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 
 // TODO: Make responsive -- hamburger menu
 export const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="navbar">
       <div className="brand-container">
@@ -10,8 +14,11 @@ export const NavBar = () => {
           Liz Marie
         </Link>
       </div>
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faXmark} />}
+      </div>
       <div className="nav">
-        <NavLink exact="true" className={'navbar-item'} to="/development" >
+        <NavLink exact="true" className={'navbar-item'} to="/development">
           Development
         </NavLink>
         <NavLink exact="true" className={'navbar-item'} to="/design">
