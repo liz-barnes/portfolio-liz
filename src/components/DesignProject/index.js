0e3859931adcf2737c761398253106443d742c15
Liz Barnes <embarnes95@gmail.com>
2023-03-10 22:48:23 -0800

    style design page, remove old grid

--- src/components/DesignProject/index.js
+++ src/components/DesignProject/index.js
@@ -11,18 +11,6 @@ export const DesignProject = () => {
       <NavBar />
       <div className="design-project-page">
         <h1>{project.title}</h1>
-        {/* <div className="grid">
-          {project.designs?.map((design) => (
-            <div className="design-container">
-              <div className="design-item">
-                <div className="image">
-                  <img src={design} alt={''} />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div> */}
-
         {project.projects?.map((proj) => (
           <div className="design-project">
             <h3>{proj.project}</h3>
