068e1986c5fbac0833da673858be5700f1dfa49d
Liz Barnes <embarnes95@gmail.com>
2023-09-12 22:44:03 -0700

    remove mobile device logic, add arrow icon for design project page

--- src/components/DesignPage/index.js
+++ src/components/DesignPage/index.js
@@ -1,32 +1,29 @@
 import { Footer } from '../Footer'
 import { NavBar } from '../NavBar'
 import projects from '../../helpers/data/designProjects'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 export const DesignPage = () => {
-  /* Storing user's device details in a variable*/
-  let details = navigator.userAgent
-
-  /* Creating a regular expression containing some mobile devices keywords to search it in details string*/
-  let regexp = /android|iphone|kindle|ipad/i
-
-  /* Using test() method to search regexp in details it returns boolean value*/
-  let isMobileDevice = regexp.test(details)
-
   return (
     <>
       <NavBar />
       <div className="projects page">
         {projects.map((proj, i) => (
           <div
-            className={
-              isMobileDevice ? 'mobile-project-container' : 'project-container'
-            }
+            className={'design-page-container'}
             key={i}
           >
-            <div className="project-content">
-              <h3>{proj.title}</h3>
-              <h6>{proj.type}</h6>
-            </div>
+              <div className="project-content">
+                <h3>{proj.title}</h3>
+                <h6>{proj.type}</h6>
+              </div>
+              <div className='project-button'>
+                <Link className="" to={`/design/${proj.url}`} state={proj}>
+                  <FontAwesomeIcon className='arrow-icon' icon={faAnglesRight} />
+                </Link>
+              </div>
           </div>
         ))}
       </div>
